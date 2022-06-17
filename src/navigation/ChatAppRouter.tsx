ae0b8150837a9e24493971b8ddd408238a9976bc
Anushka Mehta <anushka4118@gmail.com>
2022-06-17 17:28:01 -0400

    Implement login screen

--- src/navigation/ChatAppRouter.tsx
+++ src/navigation/ChatAppRouter.tsx
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 
 import { ChatAppContext } from "../providers/ChatAppProvider";
 import { Wrapper } from "../styles";
+import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
 
 const ChatAppRouter: React.FC = () => {
-  const {} = useContext(ChatAppContext);
+  const { currentUser } = useContext(ChatAppContext);
 
-  return <Wrapper>Chat App</Wrapper>;
+  return <Wrapper>{currentUser ? <HomeScreen /> : <LoginScreen />}</Wrapper>;
 };
 
 export default ChatAppRouter;
--- src/providers/ChatAppProvider.tsx
+++ src/providers/ChatAppProvider.tsx
@@ -1,8 +1,19 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+import { ChatKitty, CurrentUser } from "@chatkitty/core";
 
-interface ChatAppContext {}
+const chatkitty = ChatKitty.getInstance("55871d5a-6c11-4324-94ef-8bbc60f1a748");
 
-const initialValues: ChatAppContext = {};
+interface ChatAppContext {
+  login: (username: string) => void;
+  currentUser: CurrentUser | null;
+  loading: boolean;
+}
+
+const initialValues: ChatAppContext = {
+  login: () => undefined,
+  currentUser: null,
+  loading: false,
+};
 
 export const ChatAppContext = React.createContext(initialValues);
 
@@ -13,8 +24,35 @@ interface ChatAppContextProviderProps {
 const ChatAppContextProvider: React.FC<ChatAppContextProviderProps> = ({
   children,
 }: ChatAppContextProviderProps) => {
+  const [currentUser, setCurrentUser] = useState(initialValues.currentUser);
+  const [loading, setLoading] = useState(initialValues.loading);
+
+  const login = async (username: string) => {
+    setLoading(true);
+
+    await chatkitty.startSession({
+      username: username,
+    });
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    chatkitty.onCurrentUserChanged((user) => {
+      setCurrentUser(user);
+    });
+  }, [currentUser]);
+
   return (
-    <ChatAppContext.Provider value={{}}>{children}</ChatAppContext.Provider>
+    <ChatAppContext.Provider
+      value={{
+        login,
+        currentUser,
+        loading,
+      }}
+    >
+      {children}
+    </ChatAppContext.Provider>
   );
 };
 
